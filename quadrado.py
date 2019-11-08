69769bc981385a29258e7a0e698fb0fd7158b619
artur.justino <artur.justino@br.zurich.com>
2019-11-08 07:27:08 -0300

    Uploading some Python codes

---
+++ quadrado.py
@@ -0,0 +1,11 @@
+inputedValue = int(input("Digite o valor correspondente ao lado de um quadrado: "))
+squareArea = inputedValue * inputedValue
+perimeter = inputedValue + inputedValue + inputedValue + inputedValue
+print("Perímetro: " + str(perimeter) + " - Área: " + str(squareArea))
+
+#talvez com float seja mais correto
+#floatVersion:
+inputedValueFloat = float(input("Digite o valor correspondente ao lado de um quadrado: "))
+squareAreaFloat = inputedValueFloat * inputedValueFloat
+perimeterFloat = inputedValueFloat + inputedValueFloat + inputedValueFloat + inputedValueFloat
+print("Perímetro: " + str(perimeterFloat) + " - Área: " + str(squareAreaFloat))
